88f5cbceed555a9b3b23e70c71b5f6133816d11b
Ramon Audain <raudain123@gmail.com>
2020-05-04 16:23:52 -0700

    Set up basic connect and disconnect functionality in Chat.js

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({location}) => {
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
+    const ENDPOINT = "localhost:5000";
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        // Connect
+        socket.emit("join", {name, room}, () => {
+            
+        });
+
+        // Disconnect
+        return () => {
+            socket.emit("disconnect");
+
+            socket.off();
+        }
+    }, [ENDPOINT, location.search]);
 
     return (
         <div>
